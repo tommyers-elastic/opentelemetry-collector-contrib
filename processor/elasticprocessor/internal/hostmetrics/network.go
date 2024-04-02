ca8cb2cc55dc585ab2857f94a3e0b5b56d129037
ishleenk17 <ishleen.kaur@elastic.co>
2024-04-02 17:30:45 +0530

    Add system.network.packets

--- processor/elasticprocessor/internal/hostmetrics/network.go
+++ processor/elasticprocessor/internal/hostmetrics/network.go
@@ -7,15 +7,17 @@ import (
 
 func addNetworkMetrics(metrics pmetric.MetricSlice, resource pcommon.Resource, dataset string) error {
 	var timestamp pcommon.Timestamp
-	var inBytes, outBytes int64
+	var inBytes, outBytes, inPackets, outPackets int64
 	// iterate all metrics in the current scope and generate the additional Elastic system integration metrics
 	for i := 0; i < metrics.Len(); i++ {
-		metric1 := metrics.At(i)
-		if metric1.Name() == "system.network.io" {
-			dataPoints := metric1.Sum().DataPoints()
+		metric_network := metrics.At(i)
+		if metric_network.Name() == "system.network.io" {
+			dataPoints := metric_network.Sum().DataPoints()
 			for j := 0; j < dataPoints.Len(); j++ {
 				dp := dataPoints.At(j)
-				timestamp = dp.Timestamp()
+				if timestamp == 0 {
+					timestamp = dp.Timestamp()
+				}
 				value := dp.IntValue()
 				if direction, ok := dp.Attributes().Get("direction"); ok {
 					switch direction.Str() {
@@ -37,6 +39,40 @@ func addNetworkMetrics(metrics pmetric.MetricSlice, resource pcommon.Resource, d
 								name:      "system.network.out.bytes",
 								timestamp: timestamp,
 								intValue:  &outBytes,
+							},
+						)
+					}
+				}
+			}
+		}
+		if metric_network.Name() == "system.network.packets" {
+			dataPoints := metric_network.Sum().DataPoints()
+			for j := 0; j < dataPoints.Len(); j++ {
+				dp := dataPoints.At(j)
+				if timestamp == 0 {
+					timestamp = dp.Timestamp()
+				}
+				value := dp.IntValue()
+				if direction, ok := dp.Attributes().Get("direction"); ok {
+					switch direction.Str() {
+					case "receive":
+						inPackets = value
+						addMetrics(metrics, resource, dataset, dp,
+							metric{
+								dataType:  Sum,
+								name:      "system.network.in.packets",
+								timestamp: timestamp,
+								intValue:  &inPackets,
+							},
+						)
+					case "transmit":
+						outPackets = value
+						addMetrics(metrics, resource, dataset, dp,
+							metric{
+								dataType:  Sum,
+								name:      "system.network.out.packets",
+								timestamp: timestamp,
+								intValue:  &outPackets,
 							},
 						)
 					}
