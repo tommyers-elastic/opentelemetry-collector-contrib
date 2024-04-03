ac3180ef58501951d4b1f9e7cdfa2452f6a9d8c4
ishleenk17 <ishleen.kaur@elastic.co>
2024-04-03 11:46:19 +0530

    Add more metrics to network scrapper

--- processor/elasticprocessor/internal/hostmetrics/network.go
+++ processor/elasticprocessor/internal/hostmetrics/network.go
@@ -7,7 +7,7 @@ import (
 
 func addNetworkMetrics(metrics pmetric.MetricSlice, resource pcommon.Resource, dataset string) error {
 	var timestamp pcommon.Timestamp
-	var inBytes, outBytes, inPackets, outPackets int64
+	var inBytes, outBytes, inPackets, outPackets, inDropped, outDropped, inErrors, outErrors int64
 	// iterate all metrics in the current scope and generate the additional Elastic system integration metrics
 	for i := 0; i < metrics.Len(); i++ {
 		metric_network := metrics.At(i)
@@ -44,8 +44,7 @@ func addNetworkMetrics(metrics pmetric.MetricSlice, resource pcommon.Resource, d
 					}
 				}
 			}
-		}
-		if metric_network.Name() == "system.network.packets" {
+		} else if metric_network.Name() == "system.network.packets" {
 			dataPoints := metric_network.Sum().DataPoints()
 			for j := 0; j < dataPoints.Len(); j++ {
 				dp := dataPoints.At(j)
@@ -73,6 +72,72 @@ func addNetworkMetrics(metrics pmetric.MetricSlice, resource pcommon.Resource, d
 								name:      "system.network.out.packets",
 								timestamp: timestamp,
 								intValue:  &outPackets,
+							},
+						)
+					}
+				}
+			}
+		} else if metric_network.Name() == "system.network.dropped" {
+			dataPoints := metric_network.Sum().DataPoints()
+			for j := 0; j < dataPoints.Len(); j++ {
+				dp := dataPoints.At(j)
+				if timestamp == 0 {
+					timestamp = dp.Timestamp()
+				}
+				value := dp.IntValue()
+				if direction, ok := dp.Attributes().Get("direction"); ok {
+					switch direction.Str() {
+					case "receive":
+						inDropped = value
+						addMetrics(metrics, resource, dataset, dp,
+							metric{
+								dataType:  Sum,
+								name:      "system.network.in.dropped",
+								timestamp: timestamp,
+								intValue:  &inDropped,
+							},
+						)
+					case "transmit":
+						outDropped = value
+						addMetrics(metrics, resource, dataset, dp,
+							metric{
+								dataType:  Sum,
+								name:      "system.network.out.dropped",
+								timestamp: timestamp,
+								intValue:  &outDropped,
+							},
+						)
+					}
+				}
+			}
+		} else if metric_network.Name() == "system.network.errors" {
+			dataPoints := metric_network.Sum().DataPoints()
+			for j := 0; j < dataPoints.Len(); j++ {
+				dp := dataPoints.At(j)
+				if timestamp == 0 {
+					timestamp = dp.Timestamp()
+				}
+				value := dp.IntValue()
+				if direction, ok := dp.Attributes().Get("direction"); ok {
+					switch direction.Str() {
+					case "receive":
+						inErrors = value
+						addMetrics(metrics, resource, dataset, dp,
+							metric{
+								dataType:  Sum,
+								name:      "system.network.in.errors",
+								timestamp: timestamp,
+								intValue:  &inErrors,
+							},
+						)
+					case "transmit":
+						outErrors = value
+						addMetrics(metrics, resource, dataset, dp,
+							metric{
+								dataType:  Sum,
+								name:      "system.network.out.errors",
+								timestamp: timestamp,
+								intValue:  &outErrors,
 							},
 						)
 					}
