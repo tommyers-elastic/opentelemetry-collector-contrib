64955f7c4da1729bf67807e4003f80a1d3501f48
ishleenk17 <ishleen.kaur@elastic.co>
2024-04-26 17:21:28 +0530

    Remove commented song

--- processor/elasticprocessor/internal/hostmetrics/disk.go
+++ processor/elasticprocessor/internal/hostmetrics/disk.go
@@ -27,7 +27,6 @@ func addDiskMetrics(metrics pmetric.MetricSlice, resource pcommon.Resource, data
 					name := metric.Name()
 					timestamp := dp.Timestamp()
 					value := dp.IntValue()
-					//addDiskIntMetric(metrics, resource, dataset, name, device, direction.Str(), timestamp, value)
 					addDiskMetric(metrics, resource, dataset, name, device, direction.Str(), timestamp, value, multiplier)
 				}
 			}
@@ -50,7 +49,6 @@ func addDiskMetrics(metrics pmetric.MetricSlice, resource pcommon.Resource, data
 					if name == "system.disk.operation_time" {
 						multiplier = 1000
 					}
-					//addDiskDoubleMetric(metrics, resource, dataset, name, device, direction.Str(), timestamp, value, multiplier)
 					addDiskMetric(metrics, resource, dataset, name, device, direction.Str(), timestamp, value, multiplier)
 				}
 			}
@@ -69,7 +67,6 @@ func addDiskMetrics(metrics pmetric.MetricSlice, resource pcommon.Resource, data
 				} else {
 					continue
 				}
-				//addDiskDoubleMetric(metrics, resource, dataset, ), device, "io", timestamp, value, multiplier)
 				addDiskMetric(metrics, resource, dataset, metric.Name(), device, "", timestamp, value, multiplier)
 			}
 		} else if metric.Name() == "system.disk.pending_operations" {
@@ -88,70 +85,16 @@ func addDiskMetrics(metrics pmetric.MetricSlice, resource pcommon.Resource, data
 				var multiplier int64 = 1
 
 				addDiskMetric(metrics, resource, dataset, metric.Name(), device, "", timestamp, value, multiplier)
-				//addDiskIntMetric(metrics, resource, dataset, metric.Name(), device, "io", timestamp, value)
 			}
 		}
 	}
 	return nil
 }
 
-// Adds the translated metrics with Int Values
-func addDiskIntMetric(metrics pmetric.MetricSlice, resource pcommon.Resource,
-	dataset, name, device, esmetricname string, timestamp pcommon.Timestamp, value int64) {
-
-	metricsToAdd := map[string]string{
-		"system.disk.io":                 "system.diskio.%s.bytes",
-		"system.disk.operations":         "system.diskio.%s.count",
-		"system.disk.pending_operations": "system.diskio.%s.ops",
-	}
-
-	if metricNetworkES, ok := metricsToAdd[name]; ok {
-		attributes := pcommon.NewMap()
-		attributes.PutStr("system.diskio.name", device)
-
-		addMetrics(metrics, resource, dataset,
-			metric{
-				dataType:   Sum,
-				name:       fmt.Sprintf(metricNetworkES, esmetricname),
-				timestamp:  timestamp,
-				intValue:   &value,
-				attributes: &attributes,
-			})
-	}
-}
-
-// Adds the translated metrics with Double Values
-func addDiskDoubleMetric(metrics pmetric.MetricSlice, resource pcommon.Resource,
-	dataset, name, device, esmetricname string, timestamp pcommon.Timestamp, value float64, multiplier float64) {
-
-	metricsToAdd := map[string]string{
-		"system.disk.operation_time": "system.diskio.%s.time",
-		"system.disk.io_time":        "system.diskio.%s.time",
-	}
-
-	if metricNetworkES, ok := metricsToAdd[name]; ok {
-		attributes := pcommon.NewMap()
-		attributes.PutStr("system.diskio.name", device)
-		value = value * multiplier
-
-		addMetrics(metrics, resource, dataset,
-			metric{
-				dataType:    Sum,
-				name:        fmt.Sprintf(metricNetworkES, esmetricname),
-				timestamp:   timestamp,
-				doubleValue: &value,
-				attributes:  &attributes,
-			})
-	}
-}
-
 func addDiskMetric[T interface {
 	constraints.Integer | constraints.Float
 }](metrics pmetric.MetricSlice, resource pcommon.Resource,
 	dataset, name, device, direction string, timestamp pcommon.Timestamp, value, multiplier T) {
-
-	// func addDiskMetric(metrics pmetric.MetricSlice, resource pcommon.Resource,
-	// 	dataset, name, device, direction string, timestamp pcommon.Timestamp, value any, multiplier any) {
 
 	metricsToAdd := map[string]string{
 		"system.disk.io":                 "system.diskio.%s.bytes",
