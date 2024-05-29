5593cab5af7c0477ce67e82564b784ed7e98184c
ishleenk17 <ishleen.kaur@elastic.co>
2024-05-29 12:42:37 +0530

    Updating processor to use the opentelemetry-lib

--- processor/elasticprocessor/go.mod
+++ processor/elasticprocessor/go.mod
@@ -3,17 +3,17 @@ module github.com/tommyers-elastic/opentelemetry-collector-contrib/processor/ela
 go 1.21.1
 
 require (
+	github.com/elastic/opentelemetry-lib v0.0.0-20240520143123-3234f90c8fca
 	go.opentelemetry.io/collector/component v0.92.0
 	go.opentelemetry.io/collector/consumer v0.92.0
-	go.opentelemetry.io/collector/pdata v1.0.1
+	go.opentelemetry.io/collector/pdata v1.7.0
 	go.opentelemetry.io/collector/processor v0.92.0
-	go.uber.org/zap v1.26.0
+	go.uber.org/zap v1.27.0
 	golang.org/x/exp v0.0.0-20240416160154-fe59bbe5cc7f
 )
 
 require (
 	github.com/gogo/protobuf v1.3.2 // indirect
-	github.com/golang/protobuf v1.5.3 // indirect
 	github.com/hashicorp/go-version v1.6.0 // indirect
 	github.com/json-iterator/go v1.1.12 // indirect
 	github.com/knadh/koanf/maps v0.1.1 // indirect
@@ -33,10 +33,10 @@ require (
 	go.opentelemetry.io/otel/metric v1.21.0 // indirect
 	go.opentelemetry.io/otel/trace v1.21.0 // indirect
 	go.uber.org/multierr v1.11.0 // indirect
-	golang.org/x/net v0.19.0 // indirect
-	golang.org/x/sys v0.15.0 // indirect
+	golang.org/x/net v0.23.0 // indirect
+	golang.org/x/sys v0.18.0 // indirect
 	golang.org/x/text v0.14.0 // indirect
-	google.golang.org/genproto/googleapis/rpc v0.0.0-20231002182017-d307bd883b97 // indirect
-	google.golang.org/grpc v1.60.1 // indirect
-	google.golang.org/protobuf v1.32.0 // indirect
+	google.golang.org/genproto/googleapis/rpc v0.0.0-20240227224415-6ceb2ff114de // indirect
+	google.golang.org/grpc v1.63.2 // indirect
+	google.golang.org/protobuf v1.34.0 // indirect
 )
--- processor/elasticprocessor/processor.go
+++ processor/elasticprocessor/processor.go
@@ -4,44 +4,64 @@ import (
 	"context"
 	"strings"
 
-	"github.com/tommyers-elastic/opentelemetry-collector-contrib/processor/elasticprocessor/internal/hostmetrics"
-
+	"github.com/elastic/opentelemetry-lib/remappers/hostmetrics"
+	//	"github.com/tommyers-elastic/opentelemetry-collector-contrib/processor/elasticprocessor/internal/hostmetrics"
+	"go.opentelemetry.io/collector/pdata/pcommon"
 	"go.opentelemetry.io/collector/pdata/plog"
 	"go.opentelemetry.io/collector/pdata/pmetric"
 	"go.opentelemetry.io/collector/pdata/ptrace"
 	"go.opentelemetry.io/collector/processor"
-
 	"go.uber.org/zap"
 )
 
+type remapper interface {
+	Remap(pmetric.ScopeMetrics, pmetric.MetricSlice, pcommon.Resource)
+}
+
 type ElasticProcessor struct {
-	cfg     *Config
-	logger  *zap.Logger
-	storage map[string]any
+	cfg       *Config
+	logger    *zap.Logger
+	storage   map[string]any
+	remappers []remapper
 }
 
 func newProcessor(set processor.CreateSettings, cfg *Config) *ElasticProcessor {
-	return &ElasticProcessor{cfg: cfg, logger: set.Logger, storage: make(map[string]any)}
+	remappers := []remapper{
+		hostmetrics.NewRemapper(set.Logger, hostmetrics.WithSystemIntegrationDataset(true)),
+	}
+	return &ElasticProcessor{
+		cfg:       cfg,
+		logger:    set.Logger,
+		storage:   make(map[string]any),
+		remappers: remappers,
+	}
 }
 
 func (p *ElasticProcessor) processMetrics(_ context.Context, md pmetric.Metrics) (pmetric.Metrics, error) {
+	p.logger.Info("Starting to process metrics", zap.Int("resourceMetricsCount", md.ResourceMetrics().Len()))
 	for i := 0; i < md.ResourceMetrics().Len(); i++ {
 		resourceMetric := md.ResourceMetrics().At(i)
 		rm := resourceMetric.Resource()
 
 		for j := 0; j < resourceMetric.ScopeMetrics().Len(); j++ {
 			scopeMetric := resourceMetric.ScopeMetrics().At(j)
-
 			if p.cfg.AddSystemMetrics {
-				if strings.HasPrefix(scopeMetric.Scope().Name(), "otelcol/hostmetricsreceiver") {
-					if err := hostmetrics.AddElasticSystemMetrics(scopeMetric, rm, p.storage); err != nil {
-						p.logger.Error("error adding hostmetrics data", zap.Error(err))
+				p.logger.Info("Add System Metrics set to true")
+				if len(p.remappers) > 0 {
+					if strings.HasPrefix(scopeMetric.Scope().Name(), "otelcol/hostmetricsreceiver") {
+						//hostmetrics.AddElasticSystemMetrics(scopeMetric, rm, p.storage)
+						p.logger.Debug("Remapping metrics", zap.String("scopeName", scopeMetric.Scope().Name()))
+						for _, r := range p.remappers {
+							r.Remap(scopeMetric, scopeMetric.Metrics(), rm)
+						}
+						p.logger.Debug("Finished remapping metrics", zap.String("scopeName", scopeMetric.Scope().Name()))
 					}
+
 				}
 			}
 		}
 	}
-
+	p.logger.Info("Finished processing metrics", zap.Int("resourceMetricsCount", md.ResourceMetrics().Len()))
 	return md, nil
 }
 
