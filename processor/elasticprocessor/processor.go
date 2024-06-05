9307be3e222d333fc68ef9b3078ab6757c7db5ec
Ishleen Kaur <102962586+ishleenk17@users.noreply.github.com>
2024-06-05 15:33:55 +0530

    Elastic Infra Processor Code Refactoring (#5)
    * Network scrapper
    
    * Add network scrapper  to hostmetrics
    
    * Add system.network.packets
    
    * Add more metrics to network scrapper
    
    * Move attribute addition to the network.go file
    
    * Add code for Network Scraper
    
    * remove disk for now
    
    * comment a particular part
    
    * Have just a single metric
    
    * Uncomment the code
    
    * update process.go
    
    * Improve the code of Network Scraper
    
    * Change in network scraper code
    
    * Update the metric.attributes
    
    * Add support for disk scraper
    
    * Change the metric name
    
    * Update disk scraper
    
    * Combine Int and Float metric into one
    
    * Rectify disk.go
    
    * Remove commented song
    
    * Updating processor to use the opentelemetry-lib
    
    * Checking with false dataset setting
    
    * Remove the Hostmetrcis Code from Processor
    
    * update processor.go
    
    * Refactor the processor code
    
    * Remove the storage variable

--- processor/elasticprocessor/processor.go
+++ processor/elasticprocessor/processor.go
@@ -2,28 +2,43 @@ package elasticprocessor
 
 import (
 	"context"
-	"strings"
 
-	"github.com/tommyers-elastic/opentelemetry-collector-contrib/processor/elasticprocessor/internal/hostmetrics"
-
+	"github.com/elastic/opentelemetry-lib/remappers/hostmetrics"
+	"go.opentelemetry.io/collector/pdata/pcommon"
 	"go.opentelemetry.io/collector/pdata/plog"
 	"go.opentelemetry.io/collector/pdata/pmetric"
 	"go.opentelemetry.io/collector/pdata/ptrace"
 	"go.opentelemetry.io/collector/processor"
-
 	"go.uber.org/zap"
 )
 
+// remapper interface defines the Remap method that should be implemented by different remappers
+type remapper interface {
+	Remap(pmetric.ScopeMetrics, pmetric.MetricSlice, pcommon.Resource)
+}
+
 type ElasticProcessor struct {
-	cfg     *Config
-	logger  *zap.Logger
-	storage map[string]any
+	cfg       *Config
+	logger    *zap.Logger
+	remappers []remapper
 }
 
 func newProcessor(set processor.CreateSettings, cfg *Config) *ElasticProcessor {
-	return &ElasticProcessor{cfg: cfg, logger: set.Logger, storage: make(map[string]any)}
+	var remappers []remapper
+	// Initialize the remapper slice if AddSystemMetrics is enabled
+	if cfg.AddSystemMetrics {
+		remappers = []remapper{
+			hostmetrics.NewRemapper(set.Logger, hostmetrics.WithSystemIntegrationDataset(true)),
+		}
+	}
+	return &ElasticProcessor{
+		cfg:       cfg,
+		logger:    set.Logger,
+		remappers: remappers,
+	}
 }
 
+// processMetrics processes the given metrics and applies remappers if configured.
 func (p *ElasticProcessor) processMetrics(_ context.Context, md pmetric.Metrics) (pmetric.Metrics, error) {
 	for i := 0; i < md.ResourceMetrics().Len(); i++ {
 		resourceMetric := md.ResourceMetrics().At(i)
@@ -31,17 +46,11 @@ func (p *ElasticProcessor) processMetrics(_ context.Context, md pmetric.Metrics)
 
 		for j := 0; j < resourceMetric.ScopeMetrics().Len(); j++ {
 			scopeMetric := resourceMetric.ScopeMetrics().At(j)
-
-			if p.cfg.AddSystemMetrics {
-				if strings.HasPrefix(scopeMetric.Scope().Name(), "otelcol/hostmetricsreceiver") {
-					if err := hostmetrics.AddElasticSystemMetrics(scopeMetric, rm, p.storage); err != nil {
-						p.logger.Error("error adding hostmetrics data", zap.Error(err))
-					}
-				}
+			for _, r := range p.remappers {
+				r.Remap(scopeMetric, scopeMetric.Metrics(), rm)
 			}
 		}
 	}
-
 	return md, nil
 }
 
